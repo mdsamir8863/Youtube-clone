2abe112f1d36f05daabf96e15205d30c059a5c17
SAMIR <109948887+mdsamir8863@users.noreply.github.com>
2023-01-10 17:27:19 +0530

    first commit

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,25 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// const axios = require("axios");
+
+// const options = {
+//   method: "GET",
+//   url: "https://youtube138.p.rapidapi.com/auto-complete/",
+//   params: { q: "desp", hl: "en", gl: "US" },
+//   headers: {
+//     "X-RapidAPI-Key": "cf0f092186mshece4168047b4886p1052e2jsnd5781f3ef247",
+//     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+//   },
+// };
+
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
---
+++ src/utils/api.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+const options = {
+  url: "https://youtube138.p.rapidapi.com/auto-complete/",
+  params: { q: "youtube", hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": 'cf0f092186mshece4168047b4886p1052e2jsnd5781f3ef247',
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
+
+//   import axios from "axios";
+
+// const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+// const options = {
+//   params: { hl: "en", gl: "US" },
+//   headers: {
+//     "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+//     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+//   },
+// };
+
+// export const fetchDataFromApi = async (url) => {
+//   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+//   return data;
+// };
